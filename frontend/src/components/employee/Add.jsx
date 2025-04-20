ca9c8fb67351bd9f145025004ad5ae2e9bac6e8e
mohitsharma1923 <mohit.ramarpit@gmail.com>
2025-04-20 20:57:13 +0530

    Employee

---
+++ frontend/src/components/employee/Add.jsx
@@ -0,0 +1,234 @@
+import React, { useEffect, useState } from "react";
+import { fetchDepartments } from "../../utils/EmployeeHelpers";
+import axios from "axios";
+
+
+const Add = () => {
+  const [departments, setDepartments] = useState([]);
+  const [formData, setFormData] = useState({});
+  useEffect(() => {
+    const getDepartments = async () => {
+      const departments = await fetchDepartments();
+      setDepartments(departments);
+    };
+    getDepartments();
+  }, []);
+  const handleChange = (e) => {
+    const { name, value, files } = e.target;
+    if (name === "image") {
+      setFormData((prevData) => ({ ...prevData, [name]: files[0] }));
+    } else {
+      setFormData((prevData) => ({ ...prevData, [name]: value }));
+    }
+  };
+  const handleSubmit=async(e)=>{
+    e.preventDefault();
+
+    const formDataObj= new FormData();
+    Object.keys(formData).forEach((key)=>{
+      formDataObj.append(key,formData[key])
+    })
+
+    try {
+      const response = await axios.post('http://localhost:4500/api/employee/add',formDataObj,{
+          headers:{
+              "Authorization":`Bearer ${localStorage.getItem('token')}`
+          }
+      })
+      if(response.data.success){
+         navigate("/admin-dashboard/employees")
+      }
+  } catch (error) {
+      if(error.response && !error.response.data.success){
+          alert(error.response.data.error)
+      }
+  }
+  }
+
+  return (
+    <div className="max-w-4xl mx-auto mt-10 bg-White p-8 rounded-md shadow-md">
+      <h2 className="text-2xl font-bold mb-6"> Add New Employee</h2>
+      <form onSubmit={handleSubmit}>
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+          <div>
+            <label className="block text-sm font-medium text-gray-700">
+              Name
+            </label>
+            <input
+              type="text"
+              name="name"
+              onChange={handleChange}
+              placeholder="Enter name"
+              className="mt-1 p-2 block w-full border border-gray-300 rounded-md"
+              required
+            />
+          </div>
+          <div>
+            <label className="block text-sm font-medium text-gray-700">
+              Email
+            </label>
+            <input
+              type="email"
+              name="email"
+              onChange={handleChange}
+              placeholder="Enter Email"
+              className="mt-1 p-2 block w-full border border-gray-300 rounded-md"
+              required
+            />
+          </div>
+          <div>
+            <label className="block text-sm font-medium text-gray-700">
+              Employee ID
+            </label>
+            <input
+              type="text"
+              name="employeeId"
+              onChange={handleChange}
+              placeholder="Enter Employee ID"
+              className="mt-1 p-2 block w-full border border-gray-300 rounded-md"
+              required
+            />
+          </div>
+          <div>
+            <label className="block text-sm font-medium text-gray-700">
+              Date of Birth
+            </label>
+            <input
+              type="date"
+              name="dob"
+              onChange={handleChange}
+              placeholder="Enter DOB"
+              className="mt-1 p-2 block w-full border border-gray-300 rounded-md"
+              required
+            />
+          </div>
+          <div>
+            <label className="block text-sm font-medium text-gray-700">
+              Gender
+            </label>
+            <select
+              name="gender"
+              onChange={handleChange}
+              className="mt-1 p-2 block w-full border border-gray-300 rounded-md"
+              required
+            >
+              <option value="">Select Gender</option>
+              <option value="male">Male</option>
+              <option value="female">Female</option>
+            </select>
+          </div>
+          <div>
+            <label className="block text-sm font-medium text-gray-700">
+              Material Status
+            </label>
+            <select
+              name="maritalStatus"
+              onChange={handleChange}
+              placeholder="Enter Marital Status"
+              className="mt-1 p-2 block w-full border border-gray-300 rounded-md"
+              required
+            >
+              <option value="">Select Marital Status</option>
+              <option value="single">Single</option>
+              <option value="married">Married</option>
+            </select>
+          </div>
+          <div>
+            <label className="block text-sm font-medium text-gray-700">
+              Designation
+            </label>
+            <input
+              type="text"
+              name="designation"
+              onChange={handleChange}
+              placeholder="Enter Designation"
+              className="mt-1 p-2 block w-full border border-gray-300 rounded-md"
+              required
+            />
+          </div>
+          <div>
+            <label className="block text-sm font-medium text-gray-700">
+              Department
+            </label>
+            <select
+              name="department"
+              onChange={handleChange}
+              className="mt-1 p-2 block w-full border border-gray-300 rounded-md"
+              required
+            >
+              <option value="">Select Department</option>
+              {departments.map((dep) => (
+                <option key={dep._id} value={dep._id}>
+                  {dep.dep_name}
+                </option>
+              ))}
+            </select>
+          </div>
+          <div>
+            <label className="block text-sm font-medium text-gray-700">
+              Salary
+            </label>
+            <input
+              type="number"
+              name="salary"
+              onChange={handleChange}
+              placeholder="Enter Salary"
+              className="mt-1 p-2 block w-full border border-gray-300 rounded-md"
+              required
+            />
+          </div>
+          <div>
+            <label className="block text-sm font-medium text-gray-700">
+              Password
+            </label>
+            <input
+              type="password"
+              name="password"
+              onChange={handleChange}
+              placeholder="*******"
+              className="mt-1 p-2 block w-full border border-gray-300 rounded-md"
+              required
+            />
+          </div>
+          <div>
+            <label className="block text-sm font-medium text-gray-700">
+              Role
+            </label>
+            <select
+              name="role"
+              onChange={handleChange}
+              className="mt-1 p-2 block w-full border border-gray-300 rounded-md"
+              required
+            >
+              <option value="">Select Role</option>
+              <option value="admin">Admin</option>
+              <option value="employee">Employee</option>
+            </select>
+          </div>
+          <div>
+            <label className="block text-sm font-medium text-gray-700">
+              Upload Image
+            </label>
+            <input
+              type="file"
+              name="image"
+              onChange={handleChange}
+              placeholder="Upload Image"
+              accept="image/"
+              className="mt-1 p-2 block w-full border border-gray-300 rounded-md"
+              required
+            />
+          </div>
+        </div>
+        <button
+          type="sunmit"
+          className="w-full mt-6 bg-teal-600 hover:bg-teal-700 text-white font-bold py-2 px-4 rounded"
+        >
+          Add Employee
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default Add;
